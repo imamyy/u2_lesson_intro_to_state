67ec04a8ad7ae6d4dd52091591efde5667db50eb
imamyy <amytran2608@gmail.com>
2022-09-24 12:51:55 -0400

    Removed a task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Tasks = (props) => {
-
   return (
     <ul>
       {props.tasks.map((task, index) => (
         <li key={index}>
-          {task} 
+          {task}
+          <button onClick={() => props.removeTask(index)}>x</button>
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
